60083a459edee387723680a9b6875f8c63b4051c
unknown <aa.aityounes.anis@gmail.com>
2025-01-07 10:27:40 +0100

    Clean the CSV File

--- fetch_data/fetch_xauusd_historical_data.py
+++ fetch_data/fetch_xauusd_historical_data.py
@@ -4,6 +4,9 @@ import pandas as pd
 # Define the ticker symbol for XAUUSD
 ticker_symbol = "GC=F"
 
+# Define the File path
+file_path = "./data/xauusd_historical_data.csv"
+
 # Define the date range
 start_date = "2015-01-01"
 end_date = "2025-01-07"
@@ -14,5 +17,19 @@ data = yf.download(ticker_symbol, start=start_date, end=end_date)
 # Exclude the 'Volume' column
 data = data.drop(columns=['Volume'])
 
+
+data = pd.read_csv(file_path, skiprows=3)
+
+data.columns = ["Date", "Close", "High", "Low", "Open"]
+
+# Set the 'Date' column as the index
+data.set_index("Date", inplace=True)
+
+# Convert the index to a datetime format
+data.index = pd.to_datetime(data.index)
+
+# Save the cleaned data to a new CSV file (optional)
+data.to_csv(file_path)
+
 # Save the data to a CSV file (optional)
-data.to_csv("./data/xauusd_historical_data.csv")
+data.to_csv(file_path)
